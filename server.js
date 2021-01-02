7c9881667253774fbc457e93488d9cd5b36e841c
Remus Calugarescu <remcalu@outlook.com>
2021-01-02 05:05:42 -0500

    Basic express.js template

--- server.js
+++ server.js
@@ -1,15 +1,58 @@
-const http = require('http');
+/************************
+Required External Modules
+************************/
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const {spawn} = require('child_process');
+
+/************
+App Variables 
+************/
 const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+const port = process.env.PORT || "8000";
+
+/*****************
+Routes Definitions
+*****************/
+// Default URL for website
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+app.get('/', function(req, res) {
+ 
+    var dataToSend;
+
+    // Spawn new child process to call the python script
+    const python = spawn('python', ['webscraper.py']);
+
+    // Collect data from script
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+    });
+
+    // In close event we are sure that stream from child process is closed
+    python.on('close', function(code) {
+        console.log(`child process close all stdio with code ${code}`);
+
+        // Send data to browser
+        res.send(dataToSend)
+    });
+
+});
+
+/****************
+Server Activation
+****************/
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+/****************
+App Configuration
+****************/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
